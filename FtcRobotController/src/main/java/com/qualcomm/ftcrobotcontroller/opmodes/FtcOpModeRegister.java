e44d1a605e2383d03b1e0de49c4f25c45c309d54
Henry Agnew <henryd.agnew@gmail.com>
2015-09-25 00:22:08 -0700

    Add back Custom Teleop programs in register

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -94,7 +94,11 @@ public class FtcOpModeRegister implements OpModeRegister {
     //manager.register("PushBotIrSeek", PushBotIrSeek.class);
     //manager.register("PushBotSquare", PushBotSquare.class);
 
-    
+    /*
+    * Custom TeleOp Programs
+    */
 
+        manager.register("LinearTeleTest", LinearTeleTest.class);
+        manager.register("TwelveTests", TwelveTests.class);
   }
 }
